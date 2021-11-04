fd6603ffa658576ebab81d1576fe81aaff59b20d
Darwin Aponte <darwin.aponte@pucp.edu.pe>
2021-11-04 09:19:48 -0500

    version1

---
+++ src/pe/edu/pucp/Animal.java
@@ -0,0 +1,2 @@
+package pe.edu.pucp;public class Animal {
+}
---
+++ src/pe/edu/pucp/Main.java
@@ -0,0 +1,8 @@
+package pe.edu.pucp;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
